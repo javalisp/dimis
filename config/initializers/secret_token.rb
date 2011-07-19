19dd9ae5bbec20b14f315b1cdb18648aae062272
javalisp <javalisp@126.com>
2011-07-19 10:46:34 +0800

    prj init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DImis::Application.config.secret_token = '12dcd2a93bbc6cb0ee998fead4c129e7a67489044b7c175119fa540adeb49ce6f599ba8cf4e006489624b9611776f4955746732172fd8b2408b4df3df41ea876'
